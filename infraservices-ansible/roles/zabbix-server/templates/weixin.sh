c9933e5146286c34831cd4c9540717626d9c26a6
unknown <chengjn@siss.local>
2019-03-29 16:17:27 +0800

    微信脚本Bug修复

--- infraservices-ansible/roles/zabbix-server/templates/weixin.sh
+++ infraservices-ansible/roles/zabbix-server/templates/weixin.sh
@@ -6,20 +6,20 @@ Secret='70iY5xGoRepqx2L6PqX2PxoWE97oUKIUXspYA_hoKuI'
 GURL="https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=$CropID&corpsecret=$Secret"
 Gtoken=$(/usr/bin/curl -s -G $GURL | awk -F \" '{print $10}')
 PURL="https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=$Gtoken"
+Msg=$2
 function body() {
 local int AppID=0           # 企业号中的应用id
 local UserID=$1             # 部门成员id，zabbix中定义的微信接收者
-local Msg=$2                #过滤出zabbix中传递的第三个参数
-printf '{\n'
-printf '\t"touser": "'$UserID'",\n'
+echo '{'
+echo '"touser": "'$UserID'",'
 #printf '\t"toparty": "$PartyID",\n'
-printf '\t"msgtype": "text",\n'
-printf '\t"agentid": "'$AppID'",\n'
-printf '\t"text": {\n'
-printf '\t\t"content": "'${Msg}'"\n'
-printf '\t},\n'
-printf '\t"safe":"0"\n'
-printf '}\n'
+echo '"msgtype": "text",'
+echo '"agentid": "'$AppID'",'
+echo '"text": {\n'
+echo -e '"content": "'${Msg}'"'
+echo '},'
+echo '"safe":"0"'
+echo '}'
 }
 msg=$(body $1 $2)
 echo "${msg}"
